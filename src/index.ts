460b4298fed9bea11b1c4741f9f6d080e1db6f76
Ata Kuyumcu <ata@mm.st>
2022-04-18 14:48:23 +0300

    exported core classes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import defaultPlayers from "./players";
+import SoundPlayer from "./players/SoundPlayer";
+import PlayOptions from "./PlayOptions";
+import Sound from "./Sound";
 import SoundPlayerFactory from "./SoundPlayerFactory";
+
+export { SoundPlayer };
+export { PlayOptions };
+export { Sound };
+export { SoundPlayerFactory };
 
 export default new SoundPlayerFactory(defaultPlayers)
